d1a1120a1299097079e07cf6c100748e3e58568d
Peter Mount <peter@retep.org>
2018-10-03 21:21:02 +0100

    dummy putAcl to stop it interfering with putObject

--- acl.go
+++ acl.go
@@ -25,42 +25,53 @@ type Grant struct {
 // with the entire object which breaks the client.
 // https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectGETacl.html
 func (s *ObjectStore) getObjectAcl( r *rest.Rest ) error {
-    bucketName := r.Var( "BucketName" )
-    objectName := r.Var( "ObjectName" )
+  bucketName := r.Var( "BucketName" )
+  objectName := r.Var( "ObjectName" )
 
-    obj := &Object{}
+  obj := &Object{}
 
-  	err := s.boltService.Update( func( tx *bolt.Tx ) error {
-      b, err := s.getBucket( tx, bucketName )
-      if err != nil {
-        return err
-      }
-
-  		return obj.get( b, objectName )
-  	})
+	err := s.boltService.Update( func( tx *bolt.Tx ) error {
+    b, err := s.getBucket( tx, bucketName )
     if err != nil {
       return err
     }
 
-    // Dummy data
-    acl := &AccessControlPolicy{
-      Id: "75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a",
-      DisplayName: "mtd@amazon.com",
-      Grant: []Grant{
-        Grant{
-          Id: "75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a",
-          DisplayName: "mtd@amazon.com",
-          Permission: "FULL_CONTROL",
-        },
-      },
-    }
-
-    r.Status( 200 ).
-      AddHeader( "x-amz-id-2", "LriYPLdmOdAiIfgSm/F1YsViT1LW94/xUQxMsF7xiEb1a0wiIOIxl+zbwZ163pt7" ).
-      AddHeader( "x-amz-request-id", "0A49CE4060975EAC" ).
-      AddHeader( "Content-Length", "0" ).
-      XML().
-      Value( acl )
-
-    return nil
+		return obj.get( b, objectName )
+	})
+  if err != nil {
+    return err
   }
+
+  // Dummy data
+  acl := &AccessControlPolicy{
+    Id: "75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a",
+    DisplayName: "mtd@amazon.com",
+    Grant: []Grant{
+      Grant{
+        Id: "75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a",
+        DisplayName: "mtd@amazon.com",
+        Permission: "FULL_CONTROL",
+      },
+    },
+  }
+
+  r.Status( 200 ).
+    AddHeader( "x-amz-id-2", "LriYPLdmOdAiIfgSm/F1YsViT1LW94/xUQxMsF7xiEb1a0wiIOIxl+zbwZ163pt7" ).
+    AddHeader( "x-amz-request-id", "0A49CE4060975EAC" ).
+    AddHeader( "Content-Length", "0" ).
+    XML().
+    Value( acl )
+
+  return nil
+}
+
+// putObjectAcl dummy implementation to stop it interfeering with uploadObject
+// https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUTacl.html
+func (s *ObjectStore) putObjectAcl( r *rest.Rest ) error {
+  r.Status( 200 ).
+    AddHeader( "x-amz-id-2", "LriYPLdmOdAiIfgSm/F1YsViT1LW94/xUQxMsF7xiEb1a0wiIOIxl+zbwZ163pt7" ).
+    AddHeader( "x-amz-request-id", "0A49CE4060975EAC" ).
+    AddHeader( "Content-Length", "0" )
+
+  return nil
+}
